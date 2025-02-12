ebaf867ada4c7c7ef31b918294bb7fdb44211464
Andrae Ladores <vandraes@gmail.com>
2025-02-12 15:49:41 -0800

    Delete some things

--- code/01_script.R
+++ code/01_script.R
@@ -1,10 +1,19 @@
-## This is an R script to easily streamline the batch correction &
-## compensation part of CyTOF data
-## By Dodge Ladores, created on 02/11/2025
-# This was originally pulled from:
-# https://github.com/sguldberg/pipelines/tree/main
-# But it is altered to cater towards solely the
-# batch correction & compensation parts.
+# Script created on 02/11/2025
+
+# Load all necessary packages
+library("cyCombine")
+library("magrittr")
+library("stringr")
+library("dplyr")
+library("readxl")
+
+# create new folder for bath_corrected items
+
+## set file path to 'output folder
+output_folder <- file.path("./output/", "batch_corrected")
+
+# create new folder
+dir.create(output_folder)
 
 
 
