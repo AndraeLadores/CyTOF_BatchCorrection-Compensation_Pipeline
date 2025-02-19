3cef5f029ce08783a4a109a6e9c0d994887332ee
Andrae Ladores <vandraes@gmail.com>
2025-02-19 13:40:01 -0800

    Update format to include instructions

--- code/01_batchcorrection_script.R
+++ code/01_batchcorrection_script.R
@@ -1,14 +1,15 @@
 # Script created on 02/11/2025
-# This is for batch correction, make sure necessary packages are installed
+# This is for batch correction, make sure necessary packages are installed.
 
 # Call the function
 source("./code/functions/batch_correction.R")
 
-
-# make sure the desired paths are correct
-batch_correction(dataset_folder = "./data/raw_data/test_FCS_files/",
-           panel_xcl = "./data/panel_md_test/samples_panel_test.xlsx",
-           md_xcl = "./data/panel_md_test/samples_md_test.xlsx")
+# Make sure the desired paths are correct.
+# Use the function and replace paths if needed.
+batch_correction(
+  dataset_folder = "./data/raw_data/test_FCS_files/", # FCS files/folder
+  panel_xcl = "./data/panel_md_test/samples_panel_test.xlsx", # Panel excel file
+  md_xcl = "./data/panel_md_test/samples_md_test.xlsx") # Metadata excel file
 
 
 
--- code/02_compensation_script.R
+++ code/02_compensation_script.R
@@ -1,11 +1,13 @@
 # Script created on 2/18/25
-# This is a script for compensation.
+# This is a script for compensation, make sure necessary packages are installed.
 
 # Call the compensation function
 source("./code/functions/compensation.R")
 
-# Make sure the desired paths are correct
-compensate(FCS_files = "./data/raw_data/test_FCS_files/",
-           panel_xcl = "./data/panel_md_test/samples_panel_test.xlsx",
-           md_xcl = "./data/panel_md_test/samples_md_test.xlsx",
-           spillover_file = "./data/cytof_spillover_matrix_wDi.xlsx")
+# Make sure the desired paths are correct.
+# Use the function and replace paths if needed.
+compensate(
+  FCS_files = "./data/raw_data/test_FCS_files/", # Desired FCS files
+  panel_xcl = "./data/panel_md_test/samples_panel_test.xlsx", # Panel excel file
+  md_xcl = "./data/panel_md_test/samples_md_test.xlsx", # Metadata excel file
+  spillover_file = "./data/cytof_spillover_matrix_wDi.xlsx") # Spillover file
