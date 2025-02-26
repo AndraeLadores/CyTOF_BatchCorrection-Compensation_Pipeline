52fcbe4796c5c006e80997af8d8e6a0cbb116160
Andrae Ladores <vandraes@gmail.com>
2025-02-26 11:15:49 -0800

    Update function command

--- code/01_batchcorrection_script.R
+++ code/01_batchcorrection_script.R
@@ -7,7 +7,7 @@ source("./code/functions/batch_correction.R")
 # Make sure the desired paths are correct.
 # Use the function and replace paths if needed.
 batch_correction(
-  dataset_folder = "./data/raw_data/test_FCS_files/", # FCS files/folder
+  raw_dataset_folder = "./data/raw_data/test_FCS_files/", # FCS files/folder
   panel_xcl = "./data/panel_md_template/samples_panel_template.xlsx", # Panel excel file
   md_xcl = "./data/panel_md_template/samples_md_template.xlsx") # Metadata excel file
 
