e4514035a6f02ccca0b9db95772e841584439956
vandraes@gmail.com <dodge@Dodges-MBP.ucsf.edu>
2025-04-30 13:25:12 -0700

    Update batch correction script to cater towards shell script

--- code/01_batchcorrection_script.R
+++ code/01_batchcorrection_script.R
@@ -1,15 +1,26 @@
 # Script created on 02/11/2025
 # This is for batch correction, make sure necessary packages are installed.
 
+# Set directory (for shell script)
+setwd(dirname(getwd()))
+
 # Call the function
 source("./code/functions/batch_correction.R")
 
+# Code to let shell script use multiple parameters
+args <- commandArgs(trailingOnly = TRUE)
+
+# Setting up arguments to designated file path variable
 # Make sure the desired paths are correct.
-# Use the function and replace paths if needed.
+raw_dataset_folder <- args[1] # FCS files/folder
+panel_xcl <- args[2] # Panel excel file
+md_xcl <- args[3] # Metadata excel file
+
+# Use function
 batch_correction(
-  raw_dataset_folder = "./data/raw_data/test_FCS_files/", # FCS files/folder
-  panel_xcl = "./data/panel_md_template/samples_panel_template.xlsx", # Panel excel file
-  md_xcl = "./data/panel_md_template/samples_md_template.xlsx") # Metadata excel file
+  raw_dataset_folder = raw_dataset_folder,
+  panel_xcl = panel_xcl,
+  md_xcl = md_xcl)
 
 
 
