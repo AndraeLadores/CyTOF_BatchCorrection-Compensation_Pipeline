932f4a5fabb48346df13def6e2916d411c0acc5b
vandraes@gmail.com <dodge@Dodges-MacBook-Pro.local>
2025-05-12 15:14:21 -0700

    Set up code to accomodate shell script

--- code/r_codes/02_compensation_script.R
+++ code/r_codes/02_compensation_script.R
@@ -1,13 +1,25 @@
 # Script created on 2/18/25
 # This is a script for compensation, make sure necessary packages are installed.
 
+# Set directory (for shell script)
+setwd(dirname(getwd()))
+
 # Call the compensation function
 source("./code/r_codes/functions/compensation.R")
 
+# Code to let shell script use multiple parameters
+args <- commandArgs(trailingOnly = TRUE)
+
+# Setting up arguments to designated file path variable
 # Make sure the desired paths are correct.
-# Use the function and replace paths if needed.
+FCS_files <- args[1] # FCS files/folder
+panel_xcl <- args[2] # Panel excel file
+md_xcl <- args[3] # Metadata excel file
+spillover_file <- args[4] #spillover matrix file
+
+# Using function
 compensate(
-  FCS_files = "./data/raw_data/test_FCS_files/", # Desired FCS files
-  panel_xcl = "./data/panel_md_template/samples_panel_template.xlsx", # Panel excel file
-  md_xcl = "./data/panel_md_template/samples_md_template.xlsx", # Metadata excel file
-  spillover_file = "./data/spillover/cytof_spillover_matrix_template.xlsx") # Spillover file
+  FCS_files = FCS_files,
+  panel_xcl = panel_xcl,
+  md_xcl = md_xcl,
+  spillover_file = spillover_file)
